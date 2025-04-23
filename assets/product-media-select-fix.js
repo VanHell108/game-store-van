9452f3e77f3ddc8c42d5d545aaa2af6973c14c29
VanHell108 <80883697+VanHell108@users.noreply.github.com>
2025-04-23 20:58:13 +0300

    Hide header img in product gallery and select first existing img fix

---
+++ assets/product-media-select-fix.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+   const visibleThumbs = document.querySelectorAll('.thumbnail-list__item.slider__slide');
+   for (let thumb of visibleThumbs) {
+      if (getComputedStyle(thumb).display !== 'none') {
+         thumb.querySelector('button, a')?.click();
+         break;
+      }
+   }
+});
